c35bcd692d2b9325a74366162e6cec7fbacdc5a9
FFk <104321621+FFkmmr@users.noreply.github.com>
2023-10-13 00:59:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const sliders = document.querySelectorAll('.slide')
+
+for (const slide of sliders) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('dissolution')
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    sliders.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
